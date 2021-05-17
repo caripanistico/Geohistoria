a7100dc2342035ad111c9a3c2ea5e83b70c5e2ec
vcoopman <vicente.coopman97@gmail.com>
2021-05-17 19:33:24 -0400

    front-back connected

--- Back-End/test.py
+++ Back-End/test.py
@@ -1,25 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from flask import render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/MyDatabase"
 mongo = PyMongo(app)
+CORS(app)
 
 @app.route("/")
 def home_page():
-    return render_template("index.html",
-        name=None)
+    return render_template("index.html", name=None)
+
 @app.route("/user/<name>")
 def show_user(name):
     user = mongo.db.Files.find_one_or_404({"name": name})
+
     return render_template("index.html", name=user["name"], online=user["online"])
 
 @app.route("/register/<name>", methods=['POST'])
 def register_user(name):
-    user = mongo.db.Files.insert_one(
-        {"name": name,
-         "online": False
+    user = mongo.db.Files.insert_one({
+            "name": name,
+            "online": False
         }
     )
     return 'Recibido'
+
+
+@app.route('/test')
+def test():
+    ''' test route '''
+
+    return {
+        'id': 1,
+        'name': 'Que se cho loco'
+    }, 200
--- Front-End/src/components/Mapa.js
+++ Front-End/src/components/Mapa.js
@@ -5,14 +5,20 @@ import mapa from '../images/mapa.png'
 // importing axios
 const axios = require('axios').default;
 
+const url_backend = 'http://localhost:5000'
+
 export default class Mapa extends Component {
 
     render() {
-    	// Esto hace una peticion http (es solo un ejemplo)
-		const response = axios.get('https://jsonplaceholder.typicode.com/todos/1')
-		.then(response => 
-			console.log(response.data)
-		)
+    	// Esto hace una peticion http
+    	const url = url_backend.concat('/test')
+    	console.log(url)
+
+		const response = axios.get(url)
+			.then(function (response) {
+				// handle success
+				console.log(response.data);
+			})
 
 		// luego response va a tener los datos desde el back-end
 
