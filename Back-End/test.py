499639ccd4b4a20249bf7f720cf5139f54e9a4a4
Caripanistico <dcaripan@udec.cl>
2021-05-18 00:02:49 -0400

    arreglado imports

--- Back-End/test.py
+++ Back-End/test.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+from flask_pymongo import ObjectId, PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/MyDatabase"
